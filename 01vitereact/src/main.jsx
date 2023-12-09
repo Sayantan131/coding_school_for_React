9cfcb82e07c37350cb6e2d7bdfb0b981758328a8
Sayantan <maitysayantan111@gmail.com>
2023-12-09 20:38:50 +0530

    viteReact

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href:'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>
+        Visit Google
+    </a>
+)
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+anotherElement
   
 )
