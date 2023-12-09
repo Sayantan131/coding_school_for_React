4ec9c13641a8503f82eb8c05d73dc22d2f8c096a
Sayantan <maitysayantan111@gmail.com>
2023-12-09 20:11:28 +0530

    customReact form

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -7,8 +7,18 @@ function customRender(reactElement,container){
     container.appendChild(domElement)
     */
 
+    const domElement = document.createElement
+    (reactElement.type)
+    domElement.innerHTML = reactElement.children;
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop,reactElement.props[prop])
+        }
+        container.appendChild(domElement)
+    }
+
     
-}
+
 
 
 const reactElement = {
