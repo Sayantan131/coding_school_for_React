6b30baf95c7c8a895fee527e420edc13ad84c9c9
Sayantan <maitysayantan111@gmail.com>
2023-12-10 21:54:04 +0530

    use of hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+let  [counter,setCounter]= useState(15)
+
+const addValue = ()=>{
+  counter = counter+1;
+  setCounter(counter)
+}
+
+const removeValue = ()=>{
+  setCounter(counter -1)
+}
+  // let counter = 15
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <h2> Counter value : {counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add Value</button>
+    <br />
+    <button
+    onClick={removeValue}
+    >remove value</button>
+    </>
+  )
+}
+
+export default App
