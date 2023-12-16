dd8a1fe87aa9830075b6b3d29d213ef856ef5c4a
Sayantan <maitysayantan111@gmail.com>
2023-12-16 11:47:44 +0530

    task is complete

--- 02counterproject/src/App.jsx
+++ 02counterproject/src/App.jsx
@@ -4,14 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let  [counter,setCounter]= useState(15)
+
+  const addValue = ()=>{
+    if(counter == 20){
+      setCounter(counter)
+    }
+else{
+  counter = counter+1;
+  setCounter(counter)
+}
+    
+  }
   
+  const removeValue = ()=>{
+    if(counter == 0){
+      setCounter(counter)
+    }
+    else{
+      setCounter(counter -1)
+    }
+   
+  }
 
   return (
     <>
+      <h1>Chai aur Sayantan</h1>
       
+      <h2> Counter value : {counter}</h2>
 
-
-
+      
+      <button
+    onClick={addValue}
+    >Add Value</button>
+    <br />
+    <button
+    onClick={removeValue}
+    >remove value</button>
       
     </>
   )
