b3a8c919a7d02c8b2b4d4098e6fe70e7268f51c8
Sayantan <maitysayantan111@gmail.com>
2024-01-05 22:06:07 +0530

    using props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myobj = {
+    userName:"Sayantan",
+    age:22,
+  }
+  let myArr = [1,2,3]
 
   return (
     <>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://media.istockphoto.com/id/1383796215/photo/silhouette-of-man-holding-binoculars-on-mountain-peak-against-bright-sunlight-sky-background.jpg?s=2048x2048&w=is&k=20&c=3SUwSLi4fBw_kKrmqdpqZqyQ4K7ZCwVWaOpFg8mhW9I=" alt="" width="384" height="512"/>
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+   <h1 className='bg-green-400 rounded-xl text-black p-4 mb-4'  >Tailwind Test</h1>
+   <Card channel="ChaiaurCode" someobj = {myArr}/>
+   
     </>
   )
 }
