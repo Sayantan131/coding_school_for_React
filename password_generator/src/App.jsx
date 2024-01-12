f7e0a66804810b871243aebbadf41f06c513e21d
Sayantan <maitysayantan111@gmail.com>
2024-01-12 21:08:57 +0530

    going slow

---
+++ password_generator/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//  const [length, setlength]=useState(8)
+//  const [Password,setPassword]=useState("")
+//  const[numberAllowed,setumberAllowed]=useState(false)
+//  const[charAllowed,setcharAllowednumber]=useState(false)
+
+//  const passwordGenrator = useCallback(()=>{
+// let pass = ""
+// let str=
+// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+// if(numberAllowed) str+="0123456789";
+// if(charAllowed) str+="!@#$%&{}[]`~+_"
+// for (let i = 1; i <= array.length; i++) {
+//   char = Math.floor(Math.random().str.length+1);
+//   pass = str.charAt(char);
+  
+// }
+// setPassword(pass);
+
+
+//  },[length,numberAllowed,charAllowed,setPassword])
+
+  return (
+    <>
+    <h1 text-white>passwordGenrator</h1>
+    </>
+  )
+}
+
+export default App
