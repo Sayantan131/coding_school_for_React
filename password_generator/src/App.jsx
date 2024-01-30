61e54531713df9aa699717b86401e7bff63f82f1
Sayantan <maitysayantan111@gmail.com>
2024-01-30 23:46:45 +0530

    let's start again

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -4,26 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-//  const [length, setlength]=useState(8)
-//  const [Password,setPassword]=useState("")
-//  const[numberAllowed,setumberAllowed]=useState(false)
-//  const[charAllowed,setcharAllowednumber]=useState(false)
 
-//  const passwordGenrator = useCallback(()=>{
-// let pass = ""
-// let str=
-// "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-// if(numberAllowed) str+="0123456789";
-// if(charAllowed) str+="!@#$%&{}[]`~+_"
-// for (let i = 1; i <= array.length; i++) {
-//   char = Math.floor(Math.random().str.length+1);
-//   pass = str.charAt(char);
-  
-// }
-// setPassword(pass);
-
-
-//  },[length,numberAllowed,charAllowed,setPassword])
 
   return (
     <>
