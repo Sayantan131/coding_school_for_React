bfdaeb8c70c7be6687f8721582d4e4309bc2cd28
Sayantan <maitysayantan111@gmail.com>
2024-02-15 21:55:54 +0530

    running

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const[length, setlength] = useState(8)
+ const[numberAllowed, setNumberAllowed]=useState(false);
+ const[charAllowed, setCharAllowed] = useState(false);
+const[password, setPassword] = useState("")
+const passwordGenerator = () =>{}
 
   return (
     <>
-     <h1 >password generator</h1>
+     <h1 className='text-4xl text-center text-white'>password generator</h1>
     </>
   )
 }
