3329f3873398100d997fbc42956e6a888d93161a
Sayantan <maitysayantan111@gmail.com>
2024-02-20 00:15:37 +0530

    it's going

--- 07Footer/src/App.jsx
+++ 07Footer/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Body from "./body/body";
+// import Footer from "./footer/footer";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Footer</h1>
+      <Body/>
     </>
   )
 }
