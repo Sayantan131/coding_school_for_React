73056f62633b86b24e23f04c7707208a7a71e8bb
Sayantan <maitysayantan111@gmail.com>
2024-03-18 20:58:08 +0530

    it's going

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -7,6 +7,8 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
  
 // const router = createBrowserRouter([
@@ -37,6 +39,8 @@ const router = createBrowserRouter(
       <Route path='' element={<Home/>}/>
       <Route path='About' element={<About/>}/>
       <Route path='Contact' element={<Contact/>}/>
+      <Route path='User/:userid' element={<User/>}/>
+      <Route path='github' element={<Github/>}/>
 
     </Route>
   )
