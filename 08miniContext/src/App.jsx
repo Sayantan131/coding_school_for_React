943a28e731d91fab4c15f50be9c74f26fd9d25ff
Sayantan <maitysayantan111@gmail.com>
2024-03-30 19:52:00 +0530

    api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContext'
 
 function App() {
  
 
   return (
-    <>
+    <UserContextProvider>
    <h1>Chai or React</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
