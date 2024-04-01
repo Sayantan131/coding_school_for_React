20a51279e51c257b72f619a76d32698f36a3e9eb
Sayantan <maitysayantan111@gmail.com>
2024-04-01 21:28:09 +0530

    context api

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,14 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import UserContextProvider from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
+
+import Profile from './components/Profile'
+import Login from './components/Login'
 
 function App() {
  
 
   return (
     <UserContextProvider>
-   <h1>Chai or React</h1>
+    <h1>Chai or React</h1>
+    <Login/>
+    <Profile/>
     </UserContextProvider>
   )
 }
--- 08miniContext/src/context/UserContext.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-import UserContext from './UserContext';
-
-
