63f5b1fd76a5311e998e03b27c82c27dfa993d01
Sayantan <maitysayantan111@gmail.com>
2024-04-05 20:22:11 +0530

    themeswither

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -2,6 +2,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './context.js/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
 
@@ -34,10 +36,12 @@ function App() {
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                         {/*ThemeBtn*/}
+                        <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
                        {/*Card*/}
+                       <Card/>
                     </div>
                 </div>
             </div>
