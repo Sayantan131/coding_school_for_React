9ffa4e2f33fdd40a405d3c3c0ce751993e7ed5b7
Sayantan <maitysayantan111@gmail.com>
2024-04-05 19:42:04 +0530

    running

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './context.js/theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = ()=>{
+    setThemeMode("light")
+  }
+  
+
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("lighr","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+  
+
+  
 
   return (
-    <>
-   <h1 className='p-4 bg-pink-600 text-3xl'>Chai aur React</h1>
-    </>
+   
+<ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+
+
+
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/*ThemeBtn*/}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/*Card*/}
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
   )
 }
 
