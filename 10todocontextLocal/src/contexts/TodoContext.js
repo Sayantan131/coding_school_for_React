cfa84aef3b740507ecb63f5337d0d2afc196bf17
Sayantan <maitysayantan111@gmail.com>
2024-04-06 12:18:13 +0530

    running

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,7 +1,21 @@
 import {createContext,useContext} from 'react';
 
 export const TodoContext = createContext({
+   todo: [
+    {
+        id:1,
+        todo:"Todo msg",
+        completed: false, 
+    }
+   ],
+   addTodo: (todo)=>{
+     
+   },
+   updateTodo: (id, todo)=>{
 
+   },
+deleteTodo: (id)=>{},
+toggleComplete: (id) =>{}
 })
 
 export const useTodo = ()=>{
