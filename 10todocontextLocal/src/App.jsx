03111fb47c834a4b68232d0df5cf8d27ef27099e
Sayantan <maitysayantan111@gmail.com>
2024-04-22 15:28:04 +0530

    todo with react complete

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useLayoutEffect, useState } from 'react'
 import {TodoProvider} from './contexts'
 import './App.css'
+import { TodoForm, TodoItem } from './components'
 
 function App() {
 const [todos, setTodos] = useState([])
@@ -43,9 +44,17 @@ useEffect(()=>{
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                              <TodoItem todo={todo}/>  
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
